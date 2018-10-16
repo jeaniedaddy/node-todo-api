1dbcec419535eaedb46b2f5e339ad4e2c93614db
steve <jeaniedaddy@gmail.com>
2018-10-16 13:56:14 -0400

    test error occurs due to unique in user model.

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User',{
     email: {
         type: String,
         required: true,
         trim: true,
-        minLength: 1
-    }
+        minLength: 1 ,
+        // unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is duplicated'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength:6
+    },
+    tokens:[{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -106,9 +106,19 @@ app.patch('/Todos/:id',(req, res)=>{
     .catch((e)=>{
         res.status(400).send();
     });
-
-
 });
+
+app.post('/users', (req,res) =>{
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+    
+    user.save().then((user)=>{
+        res.send(user);
+    }).catch((e)=>{
+        res.status(401).send();
+    });
+
+ });
 
 app.listen(port, ()=>{ 
     console.log(`Server is running at port ${port}`);
